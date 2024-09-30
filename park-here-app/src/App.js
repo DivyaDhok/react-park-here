1c8b91a11ecfbae5e701b68776ddba0339f2c973
DivyaDhok <divyajoshi5715@gmail.com>
2024-09-30 14:53:19 +0530

    Added maps

---
+++ park-here-app/src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import Map from './Map';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Park Here</h1>
+      <Map />
+    </div>
+  );
+}
+
+export default App;
---
+++ park-here-app/src/Map.js
@@ -0,0 +1,33 @@
+// src/Map.js
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// Fix marker icon issue
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
+const Map = () => {
+  const position = [51.505, -0.09]; // Coordinates for London
+
+  return (
+    <MapContainer center={position} zoom={13} style={{ height: "400px", width: "100%" }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
